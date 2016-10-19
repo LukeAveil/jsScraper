e7d85788389cdbb5fe16d45c56581499522ceaee
Luke <lukeaveil@yahoo.com>
2016-10-19 11:58:39 +0100

    selects title from page

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.get('/scrape', function(req, res){
       var $ = cheerio.load(html);
       var title, release, rating;
       var json = {title : "", release : "", rating : ""};
+
+      $('.header').filter(function(){
+        var data = $(this);
+
+        title = data.children().first().text();
+        json.title = title;
+      });
     }
   });
 });
