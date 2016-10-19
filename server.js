4e4ad9d7c320a5730f8e1d4e0e35a33373214939
Luke <lukeaveil@yahoo.com>
2016-10-19 16:32:47 +0100

    selects all images from page

--- server.js
+++ server.js
@@ -1,44 +1,24 @@
 var express = require('express');
-var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
-app.get('/scrape', function(req, res){
-  url = 'http://www.imdb.com/title/tt1229340/';
+var url = 'http://www.gocardless.com';
+var images = [];
+
+app.get('/', function(req, res){
 
   request(url, function(error, response, html){
     if(!error){
       var $ = cheerio.load(html);
-      var title, release, rating;
-      var json = {title : "", release : "", rating : ""};
 
-      $('.title_wrapper').filter(function(){
-        var data = $(this);
-
-        title = data.children().first().text();
-        release = data.children().last().children().text();
-
-        json.title = title;
-        json.release = release;
+      $('img').each(function(i, image){
+        images.push($(image).attr('src'));
       });
-
-      $('.star-box-giga-star').filter(function(){
-        var data = $(this);
-
-        rating = data.text();
-
-        json.rating = rating;
-      });
-
-      fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-        console.log('File written, check project directory');
-      });
+      console.log(images);
     }
-
   });
 });
 
 app.listen('8081');
-console.log('Where the magic happens');
 exports = module.exports = app;
