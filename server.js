66e35e09b69e851b5a00d91c42855a15992f5d1e
Luke <lukeaveil@yahoo.com>
2016-10-21 11:42:51 +0100

    saves scraping results to json file

--- server.js
+++ server.js
@@ -1,46 +1,50 @@
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs');
 var app = express();
 
-var START_URL = 'http://www.gocardless.com';
-
-var images = [];
-var links = [];
-var scripts = [];
-var pagesToVisit = [];
-
 app.get('/', function(req, res){
+  var url = 'http://www.gocardless.com';
+  var pagesToVisit = [];
 
-  request(START_URL, function(error, response, html){
+  request(url, function(error, response, html){
     var $ = cheerio.load(html);
-    collectLinks($);
+    var images = [];
+    var links = [];
+    var scripts = [];
+    var json = {url : "", images : "", links : "", scripts : ""};
 
-      $('img').each(function(i, element){
-        images.push($(element).attr('src'));
-      });
+    $('img').each(function(i, element){
+      images.push($(element).attr('src'));
+    });
 
-      $('link').each(function(i, element){
-        links.push($(element).attr('href'));
-      });
+    $('link').each(function(i, element){
+      links.push($(element).attr('href'));
+    });
 
-      $('script').each(function(i, element){
-        scripts.push($(element).attr('src'));
-      });
+    $('script').each(function(i, element){
+      scripts.push($(element).attr('src'));
+    });
 
-      console.log(images);
-      console.log(links);
-      console.log(scripts);
+    json.url = url;
+    json.images = images;
+    json.links = links;
+    json.scripts = scripts;
+
+    fs.writeFile('assets.json', JSON.stringify(json, null, 4), function(err){
+      console.log('Data written to assets file, check project directory for assets.json file');
+    });
   });
+
+  function collectLinks($) {
+    var relativeLinks = $("a[href^='/']");
+
+    relativeLinks.each(function(){
+      pagesToVisit.push(url + $(this).attr('href'));
+    });
+  }
 });
-
-function collectLinks($) {
-  var relativeLinks = $("a[href^='/']");
-
-  relativeLinks.each(function(){
-    pagesToVisit.push(START_URL + $(this).attr('href'));
-  });
-}
 
 app.listen('8081');
 exports = module.exports = app;
